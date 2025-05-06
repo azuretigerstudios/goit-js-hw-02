73ac586cf67ae2e72d7dd05c74a76e50455d76c9
azuretigerstudios <azuretigerstudios@gmail.com>
2025-05-06 04:47:07 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -4,10 +4,10 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   if (totalPrice > customerCredits) {
     return "Insufficient funds!";
   } else {
-    const remainingCredits = customerCredits - totalPrice;
-    return `You ordered ${quantity} droids worth ${remainingCredits} credits!`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   }
 }
+
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
